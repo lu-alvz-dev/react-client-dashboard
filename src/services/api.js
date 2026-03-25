0b7bf3de72f956dd0c0d462da726b7f852aed089
Luis Alvarez <johnlavz34@gmail.com>
2026-03-25 13:39:55 -0500

    feat: add API service for fetching users

---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+export async function fectchUsers() {
+  //public mock API
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch users");
+  }
+
+  return response.json();
+}
